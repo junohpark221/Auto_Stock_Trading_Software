0d1dcae3f892377157c31927620cd4b590956eb4
Junoh Park <47418695+junohpark221@users.noreply.github.com>
2023-02-23 16:47:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sys
 from kiwoom.kiwoom import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QAxContainer import *
 
 class Main():
     def __init__(self):
-        print("메인 클래스입니다.")
-
         self.app = QApplication(sys.argv) # QApplication 객체 생성.
         self.kiwoom = Kiwoom()
+        self.kiwoom.show()
         self.app.exec_() # 이벤트 루프 실행.
 
 if __name__ == "__main__":
